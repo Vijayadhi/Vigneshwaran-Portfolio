beaabe38257d4bb870d866099f593cbe360553ef
Vijayadhi <venerablevignesh@gmail.com>
2024-12-09 00:03:08 +0530

    js-modified-2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,46 +6,46 @@
 * License: https://bootstrapmade.com/license/
 */
 
-(function () {
+(function() {
   "use strict";
 
   /**
    * Header toggle
    */
-  // Get the header toggle button
-  const headerToggleBtn = document.querySelector('.header-toggle');
+ // Get the header toggle button
+const headerToggleBtn = document.querySelector('.header-toggle');
 
-  // Function to toggle the header visibility and update button icon
-  function headerToggle() {
+// Function to toggle the header visibility and update button icon
+function headerToggle() {
+  const header = document.querySelector('#header');
+  if (header) {
+    header.classList.toggle('header-show');
+    headerToggleBtn.classList.toggle('bi-list');
+    headerToggleBtn.classList.toggle('bi-x');
+  }
+}
+
+// Add event listener to the toggle button if it exists
+if (headerToggleBtn) {
+  headerToggleBtn.addEventListener('click', headerToggle);
+}
+
+// Hide mobile nav when a same-page/hash link is clicked
+const navMenuLinks = document.querySelectorAll('#navmenu a');
+navMenuLinks.forEach(link => {
+  link.addEventListener('click', () => {
     const header = document.querySelector('#header');
-    if (header) {
-      header.classList.toggle('header-show');
-      headerToggleBtn.classList.toggle('bi-list');
-      headerToggleBtn.classList.toggle('bi-x');
+    if (header && header.classList.contains('header-show')) {
+      headerToggle();
     }
-  }
-
-  // Add event listener to the toggle button if it exists
-  if (headerToggleBtn) {
-    headerToggleBtn.addEventListener('click', headerToggle);
-  }
-
-  // Hide mobile nav when a same-page/hash link is clicked
-  const navMenuLinks = document.querySelectorAll('#navmenu a');
-  navMenuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      const header = document.querySelector('#header');
-      if (header && header.classList.contains('header-show')) {
-        headerToggle();
-      }
-    });
   });
+});
 
   /**
    * Toggle mobile nav dropdowns
    */
   document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function (e) {
+    navmenu.addEventListener('click', function(e) {
       e.preventDefault();
       this.parentNode.classList.toggle('active');
       this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
@@ -126,7 +126,7 @@
     new Waypoint({
       element: item,
       offset: '80%',
-      handler: function (direction) {
+      handler: function(direction) {
         let progress = item.querySelectorAll('.progress .progress-bar');
         progress.forEach(el => {
           el.style.width = el.getAttribute('aria-valuenow') + '%';
@@ -145,41 +145,41 @@
   /**
    * Init isotope layout and filters
    */
-  // document.querySelectorAll('.isotope-layout').forEach(function (isotopeItem) {
-  //   let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-  //   let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-  //   let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
 
-  //   let initIsotope;
-  //   imagesLoaded(isotopeItem.querySelector('.isotope-container'), function () {
-  //     initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-  //       itemSelector: '.isotope-item',
-  //       layoutMode: layout,
-  //       filter: filter,
-  //       sortBy: sort
-  //     });
-  //   });
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+        itemSelector: '.isotope-item',
+        layoutMode: layout,
+        filter: filter,
+        sortBy: sort
+      });
+    });
 
-  //   isotopeItem.querySelectorAll('.isotope-filters li').forEach(function (filters) {
-  //     filters.addEventListener('click', function () {
-  //       isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-  //       this.classList.add('filter-active');
-  //       initIsotope.arrange({
-  //         filter: this.getAttribute('data-filter')
-  //       });
-  //       if (typeof aosInit === 'function') {
-  //         aosInit();
-  //       }
-  //     }, false);
-  //   });
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+      filters.addEventListener('click', function() {
+        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        initIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aosInit === 'function') {
+          aosInit();
+        }
+      }, false);
+    });
 
-  // });
+  });
 
   /**
    * Init swiper sliders
    */
   function initSwiper() {
-    document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
       let config = JSON.parse(
         swiperElement.querySelector(".swiper-config").innerHTML.trim()
       );
@@ -197,7 +197,7 @@
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
-  window.addEventListener('load', function (e) {
+  window.addEventListener('load', function(e) {
     if (window.location.hash) {
       if (document.querySelector(window.location.hash)) {
         setTimeout(() => {
